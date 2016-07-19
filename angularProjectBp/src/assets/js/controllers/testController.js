176c633469b45dde424ab12dd0e9afd2c072fdc9
Jean ANDREANI <RIANDJE@MacBook-Pro-Val.local>
2016-07-19 17:17:14 +0200

    style & animation home page

--- angularProjectBp/src/assets/js/controllers/testController.js
+++ angularProjectBp/src/assets/js/controllers/testController.js
@@ -1,7 +1,7 @@
 myApp.controller('testController',['$scope', '$http', function($scope, $http){
 	
 	$scope.searchbarClicked = false;
-	$scope.data;
+	$scope.suggestions = [];
 
 	$scope.userEntry = '';
 
@@ -9,14 +9,17 @@ myApp.controller('testController',['$scope', '$http', function($scope, $http){
 		$http.get('http://private-anon-d042e0f724-bplink.apiary-mock.com/api/search/complete?q='+$scope.userEntry+'&limit=5')
 		.then(
 			function (response) {
-			$scope.data = response.data.result;
-			console.log($scope.data);
+			$scope.suggestions = response.data.result;
+			console.log($scope.suggestions);
+
+			document.getElementById("search").autocomplete({
+				source: $scope.suggestions;
+			});
 		}, 
 			function (err) {
 				console.log('lol nope');
 		});
-
-	}
+	};
 
 
 	$scope.toggleSearchbarClicked = function(){
