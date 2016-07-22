3d6f5a6d55449986b7713c87fcf16afe711ebdc0
Jean ANDREANI <RIANDJE@MacBook-Pro-Val.local>
2016-07-22 15:09:23 +0200

    work controller-result

--- angularProjectBp/src/assets/js/controllers/search-resultController.js
+++ angularProjectBp/src/assets/js/controllers/search-resultController.js
@@ -1,3 +1,21 @@
 myApp.controller('search-resultController',['$scope', '$http', function($scope, $http){
-	
+
+	$scope.userEntry = 'toto';
+	$scope.activity=[];
+
+	$http.get('http://private-anon-d042e0f724-bplink.apiary-mock.com/api/search?q='+$scope.userEntry+'&limit=5')
+		.then(
+			function (response) {
+			$scope.activity = response.data.result;
+			console.log($scope.activity);
+		},
+			function (err) {
+				console.log('Unable to retrieve data from the API :/');
+		});
+
+	// $scope.result = function() {
+
+		
+	// };
+
 }]);
