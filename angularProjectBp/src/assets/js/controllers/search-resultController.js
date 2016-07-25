5e7130d4a7490623e9c69cfc195064a42b9f1a4f
Harysson <harysson@MacBook-Pro-Phil.local>
2016-07-25 11:50:21 +0200

    appearance on the search-results almost done

--- angularProjectBp/src/assets/js/controllers/search-resultController.js
+++ angularProjectBp/src/assets/js/controllers/search-resultController.js
@@ -1,21 +1,24 @@
 myApp.controller('search-resultController',['$scope', '$http', function($scope, $http){
 
-	$scope.userEntry = 'toto';
-	$scope.activity=[];
+	$scope.userEntry = '';
+	$scope.activity=[
+		{name: "I'm so fake1"},
+		{name: "I'm so fake"},
+		{name: "I'm so fake"},
+		{name: "I'm so fake"},
+		{name: "I'm so fake"},
+		{name: "I'm so fake"},
+		{name: "I'm so fake"}
+	];
 
-	$http.get('http://private-anon-d042e0f724-bplink.apiary-mock.com/api/search?q='+$scope.userEntry+'&limit=5')
-		.then(
-			function (response) {
-			$scope.activity = response.data.result;
-			console.log($scope.activity);
-		},
-			function (err) {
-				console.log('Unable to retrieve data from the API :/');
-		});
-
-	// $scope.result = function() {
-
-		
-	// };
+	// $http.get('http://private-anon-d042e0f724-bplink.apiary-mock.com/api/search?q='+$scope.userEntry+'&limit=5')
+	// 	.then(
+	// 		function (response) {
+	// 		$scope.activity = response.data.result;
+	// 		console.log($scope.activity);
+	// 	},
+	// 		function (err) {
+	// 			console.log('Unable to retrieve data from the API :/');
+	// 	});
 
 }]);
