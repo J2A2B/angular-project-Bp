ae2843cd234ff2c73f5828c70235bf08c559fd53
Harysson <harysson@MacBook-Pro-Phil.local>
2016-07-25 15:49:15 +0200

    link between home and search results

--- angularProjectBp/src/assets/js/controllers/search-resultController.js
+++ angularProjectBp/src/assets/js/controllers/search-resultController.js
@@ -1,24 +1,26 @@
-myApp.controller('search-resultController',['$scope', '$http', function($scope, $http){
+myApp.controller('search-resultController',['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
 
-	$scope.userEntry = '';
-	$scope.activity=[
-		{name: "I'm so fake1"},
-		{name: "I'm so fake"},
-		{name: "I'm so fake"},
-		{name: "I'm so fake"},
-		{name: "I'm so fake"},
-		{name: "I'm so fake"},
-		{name: "I'm so fake"}
-	];
+	$scope.query = $routeParams.model;
+	// $scope.activity=[
+	// 	{name: "I'm so fake1"},
+	// 	{name: "I'm so fake"},
+	// 	{name: "I'm so fake"},
+	// 	{name: "I'm so fake"},
+	// 	{name: "I'm so fake"},
+	// 	{name: "I'm so fake"},
+	// 	{name: "I'm so fake"}
+	// ];
 
-	// $http.get('http://private-anon-d042e0f724-bplink.apiary-mock.com/api/search?q='+$scope.userEntry+'&limit=5')
-	// 	.then(
-	// 		function (response) {
-	// 		$scope.activity = response.data.result;
-	// 		console.log($scope.activity);
-	// 	},
-	// 		function (err) {
-	// 			console.log('Unable to retrieve data from the API :/');
-	// 	});
+	$scope.activity = [];
+
+	$http.get('http://private-anon-d042e0f724-bplink.apiary-mock.com/api/search?q='+$scope.query+'&limit=10')
+		.then(
+			function (response) {
+			$scope.activity = response.data.result;
+			console.log($scope.activity);
+		},
+			function (err) {
+				console.log('Unable to retrieve data from the API :/');
+		});
 
 }]);
