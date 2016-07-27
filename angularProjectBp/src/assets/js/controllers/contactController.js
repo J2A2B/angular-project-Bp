aba58f178933ac8d1e250e80968fbafb60e7403a
Jean ANDREANI <RIANDJE@MacBook-Pro-Val.local>
2016-07-27 17:44:01 +0200

    jean resolve bug anim + controller contact

--- angularProjectBp/src/assets/js/controllers/contactController.js
+++ angularProjectBp/src/assets/js/controllers/contactController.js
@@ -1,4 +1,29 @@
-myApp.controller('contactController',['$scope', '$http', function($scope, $http){
+myApp.controller('contactController',['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
 
+
+	// $scope.activity=[
+	// 	{name: "I'm so fake1"},
+	// 	{name: "I'm so fake"},
+	// 	{name: "I'm so fake"},
+	// 	{name: "I'm so fake"},
+	// 	{name: "I'm so fake"},
+	// 	{name: "I'm so fake"},
+	// 	{name: "I'm so fake"}
+	// ];
+
+	$scope.contact = [];
+
+	$http.get('http://private-anon-d042e0f724-bplink.apiary-mock.com/api/contacts/id')
+		.then(
+			function (response) {
+			$scope.contact = response.data.result;
+
+			console.log($scope.contact.length);
+		},
+			function (err) {
+				console.log('Unable to retrieve data from the API :/');
+		});
 
 }]);
+
+// +$scope.query+
