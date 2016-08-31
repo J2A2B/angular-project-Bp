3d10382689ab6013309d3a2f810a91c2ad406f20
Jean ANDREANI <RIANDJE@MacBook-Pro-Val.local>
2016-08-31 16:11:25 +0200

    icon services

--- src/assets/js/controllers/search-resultController.js
+++ src/assets/js/controllers/search-resultController.js
@@ -1,17 +1,9 @@
 myApp.controller('search-resultController',['$scope', '$http', '$routeParams', 'ApiFactory', 'newsFactory', function($scope, $http, $routeParams, ApiFactory, newsFactory){
 
 	$scope.query = $routeParams.id_keyword;
-	// $scope.activity=[
-	// 	{name: "I'm so fake1"},
-	// 	{name: "I'm so fake"},
-	// 	{name: "I'm so fake"},
-	// 	{name: "I'm so fake"},
-	// 	{name: "I'm so fake"},
-	// 	{name: "I'm so fake"},
-	// 	{name: "I'm so fake"}
-	// ];
 
 	$scope.activity = [];
+	$scope.activities = [];
 
 	// $http.get(ApiFactory.api + 'search?q='+$scope.query+'&limit=10')
 	$http.get(ApiFactory.api + 'keywords/'+$scope.query+'/activities')
@@ -19,6 +11,18 @@ myApp.controller('search-resultController',['$scope', '$http', '$routeParams', '
 			function (response) {
 			$scope.activity = response.data.result;
 			console.log($scope.activity);
+		},
+			function (err) {
+				console.log('Unable to retrieve data from the API :/');
+		});
+
+
+
+		$http.get(ApiFactory.api +'/activities')
+		.then(
+			function (response) {
+			$scope.activities = response.data.result;
+			console.log($scope.activities);
 		},
 			function (err) {
 				console.log('Unable to retrieve data from the API :/');
